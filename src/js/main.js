74ca164efad6eb771c63e43aa8722b37daec75c6
LUdanova <irynaludanova@gmail.com>
2021-02-16 21:33:08 +0200

    Creating a mask for entering a phone number, adding restrictions for the language for entering username and request text

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -13,4 +16,8 @@ window.addEventListener("DOMContentLoaded", () => {
   );
   sliders(".main-slider-item", "vertical");
   forms();
+  mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
+  showMoreStyles(".button-styles", ".styles-2");
 });
